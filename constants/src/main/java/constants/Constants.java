65722b880f2342e98f44b45cf0c1f067251c3fe2
Andrey Pomelov <k-udm@ya.ru>
2023-02-27 17:35:27 +0300

    Files from version 2.0 added

---
+++ constants/src/main/java/constants/Constants.java
@@ -0,0 +1,17 @@
+package constants;
+
+/**
+ * Глобальные константы
+ */
+public interface Constants {
+
+    /**
+     * IP-адрес, на котором работает сервер
+     */
+    String IP_ADDRESS = "localhost";
+
+    /**
+     * порт, на котором работает сервер
+     */
+    int PORT = 7777;
+}
